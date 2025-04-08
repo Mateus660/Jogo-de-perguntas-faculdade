a39e89115eba6af67ecb22aa6700f9f5b0b96ac0
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-04-07 21:27:33 -0300

    atualizado

---
+++ JAVASCRIPT/funcionamento-tela-inicial.js
@@ -0,0 +1,18 @@
+var nome = document.getElementById('nome')
+var enviar = document.getElementById('enviar')
+var labelnome = document.getElementById('labelnome')
+
+function nomecorr () {
+    if (nome.value.length <= 3) {
+       labelnome.innerHTML = "Pelo menos 4 carateres"
+       labelnome.style.color = "red"
+    } else {
+        labelnome.innerHTML = "Insira seu nome"
+        labelnome.style.color = "Black"
+    }
+
+function cadastrar () {
+    alert('olá mundo')
+ }
+}
+ 
