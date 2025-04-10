fb52c471358d311e288d4d5e6905063f542d57c5
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-04-09 22:49:12 -0300

    Projeto atualizado
    Atualização do projeto

--- JAVASCRIPT/funcionamento-tela-do-jogo.js
+++ JAVASCRIPT/funcionamento-tela-do-jogo.js
@@ -1,8 +1,20 @@
-var alternativas = document.querySelector('.alternativas')
 var responder = document.getElementById('responder')
 
 
-function responder() {
-    alert('ola');
-  }
+function resp() {
+  // Pega todas as alternativas (inputs com class "alternativas")
+  var alternativas = document.querySelectorAll('.alternativas');
   
+  // Percorre todas e vê qual está marcada
+  alternativas.forEach(function(alternativa) {
+      if (alternativa.checked) {
+          if (alternativa.value === "jupiter") {
+              alert("Você acertou!");
+          } else {
+              alert("Você errou!");
+          }
+      }
+  });
+}
+
+   
