66c2046f9d1b2ff8cd9c6b4e91385994e165cebe
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-04-11 18:33:03 -0300

    atualizado

--- JAVASCRIPT/funcionamento-tela-do-jogo.js
+++ JAVASCRIPT/funcionamento-tela-do-jogo.js
@@ -48,3 +48,67 @@ function resp3() {
       }
   });
 }
+
+function resp4() {
+    // Pega todas as alternativas (inputs com class "alternativas")
+ var alternativas = document.querySelectorAll('.alternativas');
+ 
+ // Percorre todas e vê qual está marcada
+ alternativas.forEach(function(alternativa) {
+     if (alternativa.checked) {
+         if (alternativa.value === "Uma proteção natural da Terra contra os raios solares.") {
+             alert("Você acertou!");
+         } else {
+             alert("Você errou!");
+         }
+     }
+ });
+}
+
+function resp5() {
+    // Pega todas as alternativas (inputs com class "alternativas")
+ var alternativas = document.querySelectorAll('.alternativas');
+ 
+ // Percorre todas e vê qual está marcada
+ alternativas.forEach(function(alternativa) {
+     if (alternativa.checked) {
+         if (alternativa.value === "Thomas Edison") {
+             alert("Você acertou!");
+         } else {
+             alert("Você errou!");
+         }
+     }
+ });
+}
+
+function resp6() {
+    // Pega todas as alternativas (inputs com class "alternativas")
+ var alternativas = document.querySelectorAll('.alternativas');
+ 
+ // Percorre todas e vê qual está marcada
+ alternativas.forEach(function(alternativa) {
+     if (alternativa.checked) {
+         if (alternativa.value === "França") {
+             alert("Você acertou!");
+         } else {
+             alert("Você errou!");
+         }
+     }
+ });
+}
+
+function resp7() {
+    // Pega todas as alternativas (inputs com class "alternativas")
+ var alternativas = document.querySelectorAll('.alternativas');
+ 
+ // Percorre todas e vê qual está marcada
+ alternativas.forEach(function(alternativa) {
+     if (alternativa.checked) {
+         if (alternativa.value === "Estribo") {
+             alert("Você acertou!");
+         } else {
+             alert("Você errou!");
+         }
+     }
+ });
+}
