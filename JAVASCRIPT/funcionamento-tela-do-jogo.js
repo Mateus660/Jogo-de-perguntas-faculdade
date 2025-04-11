d4fd52b76cdf560d8fc71cb5f8a463e22e2f0cba
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-04-10 22:24:03 -0300

    atualizado

--- JAVASCRIPT/funcionamento-tela-do-jogo.js
+++ JAVASCRIPT/funcionamento-tela-do-jogo.js
@@ -33,3 +33,18 @@ function afirmar() {
   });
 }
    
+function respond() {
+     // Pega todas as alternativas (inputs com class "alternativas")
+  var alternativas = document.querySelectorAll('.alternativas');
+  
+  // Percorre todas e vê qual está marcada
+  alternativas.forEach(function(alternativa) {
+      if (alternativa.checked) {
+          if (alternativa.value === "terra") {
+              alert("Você acertou!");
+          } else {
+              alert("Você errou!");
+          }
+      }
+  });
+}
