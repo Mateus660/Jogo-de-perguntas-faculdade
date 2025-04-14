58337e973f0af6fc1ac37b8b80d83813c44e5e9f
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-04-14 13:55:19 -0300

    atualizado

--- JAVASCRIPT/explicação.js
+++ JAVASCRIPT/explicação.js
@@ -1,3 +1,12 @@
 function jogar () {
     location.href = "tela-do-jogo.html"
 }
+
+window.onload = function () {
+    let nomesSalvos = JSON.parse(localStorage.getItem('nomes')) || [];
+    let ultimoNome = nomesSalvos[nomesSalvos.length - 1]; // Pega o último nome inserido
+
+    if (ultimoNome) {
+        document.getElementById('boasvindas').innerHTML = `Bem vindo, ${ultimoNome}!`;
+    }
+};
